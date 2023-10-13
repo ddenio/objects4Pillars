ba3396a77928b24d840a98a684ed501002b93e5a
Derin DeNio <derindenio@gmail.com>
2023-10-13 13:02:22 -0600

    Converted Object literal to a Factory Function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-//object literal syntax
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
-        console.log('draw circle');
-    }
-};
+// //object literal syntax
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     draw: function() {
+//         console.log('draw circle');
+//     }
+// };
 
-//dot notation
+// //dot notation
 
+// circle.draw();
+
+//Factories / constructor function
+    //Factory Function
+function createCircle(radius) {
+    return {
+        //with ES6 if we have radius = radius, we can drop the value and just keep the property
+        radius,
+        draw: function() {
+            console.log('draw circle');
+        }
+    };
+}
+
+const circle = createCircle(1);
 circle.draw();
