83797a959d6881cb45f1fe08375d85990f850142
Derin DeNio <derindenio@gmail.com>
2023-10-13 13:07:39 -0600

    created a constructor function from our object literal

--- js/main.js
+++ js/main.js
@@ -14,17 +14,32 @@
 
 // circle.draw();
 
-//Factories / constructor function
-    //Factory Function
-function createCircle(radius) {
-    return {
-        //with ES6 if we have radius = radius, we can drop the value and just keep the property
-        radius,
-        draw: function() {
-            console.log('draw circle');
-        }
-    };
+// //Factories / constructor function
+//     //Factory Function
+// function createCircle(radius) {
+//     return {
+//         //with ES6 if we have radius = radius, we can drop the value and just keep the property
+//         radius,
+//         draw: function() {
+//             console.log('draw circle');
+//         }
+//     };
+// }
+
+// const circle = createCircle(1);
+// circle.draw();
+
+
+// Constructor Function
+   //first letter of assignment should be uppercase
+function Circle(radius) {
+    // 'this' is a reference to the object that will execute this code
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw circle');
+    }
 }
 
-const circle = createCircle(1);
-circle.draw();
+const another = new Circle(1);
+
+another.draw()
